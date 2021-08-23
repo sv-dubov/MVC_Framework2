29750153ceb0e83801a8483a24f8861b7a47e0e1
Sviatoslav <sv_dubov@ukr.net>
2021-08-23 21:01:32 +0300

    Make user login

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -107,10 +107,25 @@ class Users extends Controller
             if (empty($data['password'])) {
                 $data['password_err'] = 'Please, enter password';
             }
+            // Check for user/email
+            if ($this->userModel->findUserByEmail($data['email'])) {
+                // User found
+            } else {
+                // User not found
+                $data['email_err'] = 'User not found';
+            }
             // Make sure errors are empty
             if (empty($data['email_err']) && empty($data['password_err'])) {
                 // Validated
-                die('SUCCESS');
+                // Check and set logged in user
+                $loggedInUser = $this->userModel->login($data['email'], $data['password']);
+                if ($loggedInUser) {
+                    // Create Session
+                    $this->createUserSession($loggedInUser);
+                } else {
+                    $data['password_err'] = 'Password incorrect';
+                    $this->view('users/login', $data);
+                }
             } else {
                 // Load view with errors
                 $this->view('users/login', $data);
@@ -126,5 +141,13 @@ class Users extends Controller
             // Load view
             $this->view('users/login', $data);
         }
+    }
+
+    public function createUserSession($user)
+    {
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['user_email'] = $user->email;
+        $_SESSION['user_name'] = $user->name;
+        redirect('pages/index');
     }
 }
