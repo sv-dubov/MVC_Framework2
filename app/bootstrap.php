cc91d9d2fc939ae87e36c365e5a69c3a60135bf3
Sviatoslav <sv_dubov@ukr.net>
2021-08-23 20:37:01 +0300

    Make user registration

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,8 @@
 <?php
 // Load Config
 require_once 'config/config.php';
+// Load URL Helper
+require_once 'helpers/url_helper.php';
 // Autoload Core
 spl_autoload_register(function ($className) {
     require_once 'core/' . $className . '.php';
--- app/models/User.php
+++ app/models/User.php
@@ -9,6 +9,22 @@ class User
         $this->db = new Database;
     }
 
+    // Register user
+    public function register($data)
+    {
+        $this->db->query('INSERT INTO users (name, email, password) VALUES(:name, :email, :password)');
+        // Bind values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':password', $data['password']);
+        // Execute
+        if ($this->db->execute()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // Find user by email
     public function findUserByEmail($email)
     {
