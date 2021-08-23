6dd6144e5c94b97c50f3f4a576b5e627c7ec4197
Sviatoslav <sv_dubov@ukr.net>
2021-08-23 13:17:38 +0300

    Make Base Controller

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,6 +1,6 @@
 <?php
 
-class Pages
+class Pages extends Controller
 {
     public function __construct()
     {
@@ -9,7 +9,7 @@ class Pages
 
     public function index()
     {
-
+        $this->view('hi');
     }
 
     public function about($id)
