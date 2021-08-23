a1844f21a2b4ab273c49d5e09c8649014d51ed3b
Sviatoslav <sv_dubov@ukr.net>
2021-08-23 20:19:27 +0300

    Make  User model and check email if taken

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -4,7 +4,7 @@ class Users extends Controller
 {
     public function __construct()
     {
-
+        $this->userModel = $this->model('User');
     }
 
     public function register()
@@ -28,6 +28,11 @@ class Users extends Controller
             // Validate Email
             if (empty($data['email'])) {
                 $data['email_err'] = 'Please, enter email';
+            } else {
+                // Check email
+                if ($this->userModel->findUserByEmail($data['email'])) {
+                    $data['email_err'] = 'Email is already taken';
+                }
             }
             // Validate Name
             if (empty($data['name'])) {
